b59d7efbc7e924bf1cd0c3f4b407a2fa5a781d46
Dedeepya-M <dedeepyamogilisetti@gmail.com>
2024-01-07 22:27:50 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+import gym
+import numpy as np  # Make sure to import numpy
+
+env = gym.make("CartPole-v1", render_mode="human")
+
+episodes = 10
+
+for episode in range(1, episodes + 1):
+    state = env.reset()
+    done = False
+    score = 0
+    while not done:
+        action = random.choice([0, 1])
+        obs, reward, terminated, truncated , info = env.step(action)
+        done = terminated or truncated  
+        score += reward
+        env.render()
+
+    print(f"episode {episode}, score: {score}")
+
+env.close()
